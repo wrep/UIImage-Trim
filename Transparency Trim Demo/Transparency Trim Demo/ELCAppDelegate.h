d9ad55fd973f70d434b93aa0dc9b4ea107b7c250
Chris Stroud <clstroud@ncsu.edu>
2013-06-26 11:42:56 -0400

    Added more flexibility, improved support for scale handling
    Separated the inset calculation as it alone was needed for my summer
    research project, but other changes I made in order to better support
    retina displays seemed worthy of contributing back.
    
    Now an optional boolean flag can be passed to allow the image to be
    cropped based on whether pixels are semi-transparent. See the demo
    Xcode project for an example.

---
+++ Transparency Trim Demo/Transparency Trim Demo/ELCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ELCAppDelegate.h
+//  Transparency Trim Demo
+//
+//  Created by Chris Stroud on 6/26/13.
+//  Copyright (c) 2013 Chris Stroud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ELCViewController;
+
+@interface ELCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ELCViewController *viewController;
+
+@end
